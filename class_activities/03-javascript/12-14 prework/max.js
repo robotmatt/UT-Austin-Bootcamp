7b447d7c164036eaea3cbc0985bf1246d8493764
Matt Martin <matt@incgn.to>
2019-12-14 11:24:50 -0600

    drinklist solved

---
+++ class_activities/03-javascript/12-14 prework/max.js
@@ -0,0 +1,12 @@
+//Write a function that takes in two numbers and outputs the max (the greater of the two numbers).
+//Ex:
+//Input: 1, 2
+//Output: 2
+//Input: 6, -4
+//Output: 6
+//Input: 3.4, 2
+//Output: 3.4
+
+var max = function(num1, num2){
+    return Math.max(num1, num2);
+}
