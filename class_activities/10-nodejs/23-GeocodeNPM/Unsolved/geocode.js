10bd6d94d1ffd6c09df6e2fd32f2964b826c5ac1
Matt Martin <matt@enigma.local>
2020-02-08 09:12:18 -0800

    solution

--- class_activities/10-nodejs/23-GeocodeNPM/Unsolved/geocode.js
+++ class_activities/10-nodejs/23-GeocodeNPM/Unsolved/geocode.js
@@ -11,11 +11,44 @@ var weather = require("weather-js");
 // Replace with your mapquest consumer API key
 var options = {
   provider: "mapquest",
-  apiKey: "YOUR-MAPQUEST-API-CONSUMER-KEY"
+  apiKey: "YlrspS1OmgD5qrGa9OlmkHVPl5UphQiX"
 };
 // Create a geocoder object that can query the mapquest API
 var geocoder = NodeGeocoder(options);
 // Take in the command line arguments
 // Build your address as an array or string
+var address = "";
+process.argv.forEach(function (element, index) {
+  (index > 1) ? address += element + " " : null;
+});
+console.log(address);
+
 // Then use the geocoder object to search the address
-// Add weather API call
+
+geocoder.geocode(address, function (err, data) {
+  // If there is an error log it.
+  if (err) {
+    console.log(err);
+  }
+  console.log(data);
+  // Add weather API call
+  // Then we use the package to search for the weather at a location
+
+  weather.find({
+    search: data[0].zipcode,
+    degreeType: "F"
+  }, function (err, result) {
+
+    // If there is an error log it.
+    if (err) {
+      console.log(err);
+    }
+
+    // If there is no error... then print out the weather data.
+    // We use JSON.stringify to print the data in string format.
+    // We use the JSON.stringify argument of "2" to make the format pretty.
+    // See link here: http://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
+    console.log(JSON.stringify(result, null, 2));
+
+  });
+});
